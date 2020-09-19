9d0ffa35e6a54037a9bbdab28678237b12915b2d
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-18 22:30:30 -0300

    as primeiras programaçoes

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Gritador_Etec_AT_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NomeDoUsuario;
+
+            Console.Write("Por Favor, digite seu nome: ".ToUpper());
+            NomeDoUsuario = Console.ReadLine();
+            
+            Console.Write("Obrigado, ".ToUpper());
+            Console.Write(NomeDoUsuario .ToUpper());
+
+            Console.WriteLine("!! Clique em qualquer tecla para finalizar...".ToUpper());
+            Console.ReadLine();
+            
+            
+
+
+
+
+        }
+    }
+}
